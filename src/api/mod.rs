41647c835be1ee8b95a6cee1adcc1129f304882d
DanilaRostovtsev <rostovtsev.danila@gmail.com>
2021-06-23 14:44:23 +0400

    Add endpoint get_weather_week_ahead

--- src/api/mod.rs
+++ src/api/mod.rs
@@ -5,6 +5,38 @@ use crate::services::weatherapicom::WeatherAPICom;
 use crate::EnvData;
 use std::sync::Arc;
 use actix_web::web::Query;
+use std::collections::BTreeMap;
+use chrono::NaiveDate;
+
+
+fn intersect_maps_by_key(maps: &Vec<BTreeMap<NaiveDate, f64>>) -> BTreeMap<NaiveDate, f64> {
+    let mut max_map: BTreeMap<NaiveDate, f64> = maps[0].clone();
+    let mut res_map: BTreeMap<NaiveDate, f64> = BTreeMap::new();
+
+    for v in maps {
+        if v.len() > max_map.len() {
+            max_map = v.clone();
+        }
+    }
+
+    for date in max_map.keys() {
+        let mut divider = 1;
+        let avg = maps.iter().map(|v| {
+            match v.get(date) {
+                Some(t) => {
+                    divider += 1;
+                    t.clone()
+                },
+                None => max_map.get(date).unwrap().clone()
+            }
+        })
+            .collect::<Vec<f64>>()
+            .iter().sum::<f64>() / maps.len() as f64;
+        res_map.insert(date.clone(), avg);
+    }
+
+    res_map
+}
 
 
 #[derive(Deserialize)]
@@ -12,18 +44,26 @@ struct QueryParams {
     city: String
 }
 
-
 pub struct NetologyTTApi {}
 
 impl NetologyTTApi {
-    pub async fn get_weather_today(req: HttpRequest, env_data: web::Data<Arc<EnvData>>) -> impl Responder {
+    pub async fn get_weather_current(req: HttpRequest, env_data: web::Data<Arc<EnvData>>) -> impl Responder {
         let query_params = Query::<QueryParams>::from_query(&req.query_string()).unwrap();
-        let weather_1 = OpenWeatherMap::get_weather_current(query_params.city.clone(), env_data.openweathermap_key.clone()).unwrap();
-        let weather_2 = WeatherAPICom::get_weather_current(query_params.city.clone(), env_data.weatherapicom_key.clone()).unwrap();
-        HttpResponse::Ok().json(json!({"weather_today_c": (weather_1.temperature() + weather_2.temperature()) / 2.0}))
+        let weather = vec![
+            OpenWeatherMap::get_weather_current(query_params.city.clone(), env_data.openweathermap_key.clone()).unwrap().temperature(),
+            WeatherAPICom::get_weather_current(query_params.city.clone(), env_data.weatherapicom_key.clone()).unwrap().temperature(),
+        ];
+        let res_weather = weather.iter().sum::<f64>() / weather.len() as f64;
+        HttpResponse::Ok().json(json!({"current_weather": res_weather}))
     }
 
-    pub async fn get_weather_week_ahead() -> impl Responder {
-        HttpResponse::Ok()
+    pub async fn get_weather_week_ahead(req: HttpRequest, env_data: web::Data<Arc<EnvData>> ) -> impl Responder {
+        let query_params = Query::<QueryParams>::from_query(&req.query_string()).unwrap();
+        let weathers = vec![
+            WeatherAPICom::get_weather_week_ahead(query_params.city.clone(), env_data.weatherapicom_key.clone()).unwrap(),
+            OpenWeatherMap::get_weather_week_ahead(query_params.city.clone(), env_data.openweathermap_key.clone()).unwrap(),
+        ];
+        let res_weathers = intersect_maps_by_key(&weathers);
+        HttpResponse::Ok().json(res_weathers)
     }
 }
--- src/services/openweathermap.rs
+++ src/services/openweathermap.rs
@@ -1,18 +1,38 @@
-use crate::services::{WeatherService, Weather};
-use chrono::{Local, DateTime};
-use std::collections::HashMap;
+use crate::services::{WeatherService, WeatherCurrent};
+use chrono::{Local, NaiveDate, NaiveDateTime};
+use std::collections::{HashMap, BTreeMap};
 
 pub struct OpenWeatherMap {}
 
+#[derive(Deserialize, Clone)]
+struct NominatimResponse {
+    lon: String,
+    lat: String,
+}
+
+fn get_city_coords(city: String) -> NominatimResponse {
+    let mut query_params = HashMap::new();
+    query_params.insert("q", city);
+    query_params.insert("format", "json".to_string());
+
+    reqwest::blocking::Client::new().get("http://nominatim.openstreetmap.org/search")
+        .query(&query_params)
+        .send()
+        .unwrap()
+        .json::<Vec<NominatimResponse>>()
+        .unwrap().get(0).unwrap().clone()
+}
+
+
 impl WeatherService for OpenWeatherMap {
-    fn get_weather_current(city: String, s_key: String) -> Result<Weather, ()> {
-        #[derive(Deserialize, Serialize, Debug, Clone)]
+    fn get_weather_current(city: String, s_key: String) -> Result<WeatherCurrent, ()> {
+        #[derive(Deserialize)]
         struct OpenWeatherMapResponse {
-            main: OpenWeatherMapMain,
+            main: Main,
         }
 
-        #[derive(Deserialize, Serialize, Debug, Clone)]
-        struct OpenWeatherMapMain {
+        #[derive(Deserialize)]
+        struct Main {
             temp: f64
         }
 
@@ -28,19 +48,52 @@ impl WeatherService for OpenWeatherMap {
             .unwrap();
 
         Ok(
-            Weather {
-                date: Local::now(),
+            WeatherCurrent {
+                datetime: Local::now(),
                 temperature: res.main.temp - 273.15
             }
         )
     }
 
-    fn get_weather_to_special_day(date: DateTime<Local>, city: String, s_key: String) -> Result<Weather, ()> {
-        todo!()
-    }
 
-    fn get_weather_week_ahead(city: String, s_key: String) -> Result<Vec<Weather>, ()> {
-        todo!()
+    fn get_weather_week_ahead(city: String, s_key: String) -> Result<BTreeMap<NaiveDate, f64>, ()> {
+        let coords = get_city_coords(city);
+
+        #[derive(Deserialize)]
+        struct OpenWeatherMapResponse {
+            daily: Vec<Daily>,
+        }
+
+        #[derive(Deserialize)]
+        struct Daily {
+            dt: i64,
+            temp: Temp,
+        }
+
+        #[derive(Deserialize)]
+        struct Temp {
+            day: f64,
+        }
+
+        let mut query_params = HashMap::new();
+        query_params.insert("lat", coords.lat);
+        query_params.insert("lon", coords.lon);
+        query_params.insert("exclude", "current,minutely,hourly,alerts".to_string());
+        query_params.insert("units", "metric".to_string());
+        query_params.insert("appid", s_key);
+
+        let res = reqwest::blocking::Client::new().get("https://api.openweathermap.org/data/2.5/onecall")
+            .query(&query_params)
+            .send()
+            .unwrap()
+            .json::<OpenWeatherMapResponse>()
+            .unwrap();
+
+        let weathers = res.daily.iter().map(|a|{
+            (NaiveDateTime::from_timestamp(a.dt, 0).date(), a.temp.day)
+        }).collect::<BTreeMap<NaiveDate, f64>>();
+
+        Ok(weathers)
     }
 }
 
